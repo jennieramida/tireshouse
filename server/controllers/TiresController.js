8114a4b2d73a033b617b587cb36a8192cb21c1f3
tatatatam <tam_onjoy@hotmail.com>
2018-07-23 20:16:45 +0700

    'newapi'

--- server/controllers/TiresController.js
+++ server/controllers/TiresController.js
@@ -1,18 +1,38 @@
 const Tires = require('../models/TiresControl');
 
 exports.search = (req,res,next) => {
-  console.log(req.query.width_f, req.query.series_f, req.query.diameter_f);
+  // console.log(req.query);
     // res.end();
   Tires.searchTireTypeFront(req.query.width_f, req.query.series_f, req.query.diameter_f)
   .then(front_wheel => {
-    Tires.searchTireTypeBack(req.query.width_b, req.query.series_b, req.query.diameter_b)
+    (req.query.width_b !== '' && req.query.series_b!=='' && req.query.diameter_b!=='')?
+    (Tires.searchTireTypeBack(req.query.width_b, req.query.series_b, req.query.diameter_b)
     .then(back_wheel=> {
-      const array_output = []; 
-      array_output[0] = front_wheel;
-      array_output[1] = back_wheel;
-      res.json(array_output);
+      const array_match_data = {};
+      /* make brand on front wheel */
+      for(var i=0;i<front_wheel.length;i+=1){
+        if(!(front_wheel[i].brand in array_match_data)){
+          array_match_data[front_wheel[i].brand] = {"front":[],"back":[]};
+        }
+      }
+      /* make brand on back wheel */
+      for (var i = 0; i < back_wheel.length; i += 1) {
+        if (!(back_wheel[i].brand in array_match_data)) {
+          array_match_data[back_wheel[i].brand] = { "front": [], "back": []};
+        }
+      }
+     /* fetch data to array front step*/
+      for(var i=0;i<front_wheel.length;i+=1){
+        array_match_data[front_wheel[i].brand]["front"].push(front_wheel[i])
+      }
+      /* fetch data to array back step*/
+      for (var i = 0; i < back_wheel.length; i += 1) {
+         array_match_data[back_wheel[i].brand]["back"].push(back_wheel[i])
+      }
+      
+      res.json(array_match_data);
     })
-    .then(next);
+    .catch(next)):res.json([front_wheel]);
   })
   .catch(next);
   
