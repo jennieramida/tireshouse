54145e134b45d370610b533cb383d572ec9e050e
tatatatam <tam_onjoy@hotmail.com>
2018-08-11 23:27:46 +0700

    'forresult'

--- server/routes/api/tirescontrol.js
+++
@@ -1,9 +0,0 @@
-const router = require('express').Router();
-const TiresController = require('../../controllers/TiresController');
-
-router.get('/search', TiresController.search);
-
-router.get('/info', TiresController.info);
-
-// router.get('/info', TiresController.info);
-module.exports = router;
